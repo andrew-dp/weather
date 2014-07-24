5b2fb72a94ded22173d6ea391b4ef9aaae4833d5
Andrew Guard <aguard@digitalprimates.net>
2014-07-24 18:36:42 -0500

    iterating through mongodb

--- app/scripts/controllers/LoginController.js
+++ app/scripts/controllers/LoginController.js
@@ -3,25 +3,60 @@
  */
 
 angular.module('weatherApp')
-    .controller('LoginCtrl', ['$scope', '$route', '$routeParams', '$location', function($scope, $route, $routeParams, $location) {
+    .controller('LoginCtrl', ['$q', '$http', '$scope', '$route', '$routeParams', '$location', function($q, $http, $scope, $route, $routeParams, $location) {
 
-        $scope.user = {
+        $scope.login = {
             name: '',
             password: ''
         };
 
-        $scope.auth = function() {
+        function getUserData() {
+            // get users
+            var deferred = $q.defer();
+            $http.get('http://localhost:3000/collections/weatherAppData').then(function(response) {
+                deferred.resolve(response.data);
+            });
+//                .success(function(users) {
+//                    deferred.resolve(users);
+//                })
+//                .error(function(error) {
+//                    console.error('Error: ' + error);
+//                });
+            return deferred.promise;
+        }
 
-            if ( $scope.user.name === 'wat' ) {
-                if ( $scope.user.password === 'woo' ) {
-                    $location.path('/map');
-                } else {
-                    $location.path('/');
-                    alert('password must be woo');
+        // get http://localhost:3000/collections/users
+        $scope.userLogin = function() {
+
+            getUserData().then(function(users) {
+                for ( var i = 0; i < users.length; i++ ) {
+                    console.log("user: " + users[i].name);
                 }
-            } else {
-                $location.path('/');
-                alert('username must be wat');
-            }
+            });
+
+
+
+//            console.log(userData);
+//            if ( $scope.user.name === 'wat' ) {
+//                if ( $scope.user.password === 'woo' ) {
+//                    $location.path('/map');
+//                } else {
+//                    $location.path('/');
+//                    alert('password must be woo');
+//                }
+//            } else {
+//                $location.path('/');
+//                alert('username must be wat');
+//            }
         };
+
+        $scope.signup = {
+            name: '',
+            passowrd: ''
+        };
+
+//        // post http://localhost:3000/collections/users
+        $scope.userSignup = function() {
+
+        }
     }]);
